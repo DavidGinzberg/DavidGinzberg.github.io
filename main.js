4abb7acec29af61b1f2f1bd6ce60b31196dd260f
David Ginzberg <arguing.with.my.keyboard@gmail.com>
2014-08-21 11:02:05 -0400

    Finished nav bar v1
    closes #4
    Added css to allow nav bar to stay on top without occluding section
    headings. Finished js to create all links with proper hrefs.

--- main.js
+++ main.js
@@ -11,20 +11,27 @@ $(document).ready(function(){
 	.append($("<ul/>", {
 		class:	"navLinksList"
 	})
+	// Add the "Main" or "Home" link to the nav bar
 	.append($("<li/>", {
-		class:	"navLink",
+		class:	"navLink"
+	}).append($("<a/>", {
+		href:	"#mainPageDiv",
 		text:	"Home"
-	})
-	//TODO: Add the "Main" or "Home" link to the nav bar
+	}))
+	
 	)));
 	
 	//TODO: Add a link for each topic
 	$(".topicDiv").each(function(index){
 		
 		$("<li/>", {
-			class:	"navLink",
+			class:	"navLink"
+		} )
+		.append($("<a/>", {
+			href:	"#"+$(this).attr("id"),
 			text:	$(this).data("link-name")
-		} ).appendTo(navBar.find(".navLinksList"));
+		}))
+		.appendTo(navBar.find(".navLinksList"));
 	});
 	// add the nav bar to the top of the page
 	$("body").prepend(navBar);
